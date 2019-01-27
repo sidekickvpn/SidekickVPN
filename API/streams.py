d9cd02ff8668d305d69504207ce4240d836c47c4
Luke Baal <luke.baal@uoit.net>
2019-01-27 15:17:49 -0500

    Misc node server changes, fixed fcn call in streams.py to match new naming convention

--- API/streams.py
+++ API/streams.py
@@ -16,7 +16,7 @@ try:
 
     print("Client set to: {}".format(client.public_key))
 
-    client.set_callback(client.print_outgoing_packets)
+    client.set_callback(client.print_response_packets)
     client.read_packets(mode="out")
     # client.save_incoming_packets("pkts.pcap")
     # client.parse_incoming_pcap("pkts.pcap")
