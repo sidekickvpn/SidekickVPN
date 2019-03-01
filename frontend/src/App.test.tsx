e0d04f9795ebad4e3ebb9d5305161ad4b59880b1
= <luke.baal@uoit.net>
2019-03-01 09:20:25 -0500

    Fixed autodeploy docker-compose not pulling image from registry

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+  // const AppComponent = renderer.create(<App />).toJSON();
+  // expect(AppComponent).toMatchSnapshot();
 });
--- node_server/test/app.js
+++ node_server/test/app.js
@@ -7,31 +7,17 @@ chai.use(chaiHttp);
 
 const server = require('../app');
 
-describe('GET /', () => {
-	it('should respond with welcome msg and 4 routes', done => {
-		chai.request(server)
-			.get('/')
-			.end((err, res) => {
-				should.not.exist(err);
-				res.status.should.equal(200);
-				res.type.should.equal('application/json');
-				res.body.welcome.should.eql('VPN Traffic Analysis API');
-				res.body.routes.length.should.eql(4);
-				done();
-			})
-	})
-});
-
 describe('GET /client/add', () => {
-	it('should get the add client form', done => {
-		chai.request(server)
-			.get('/client/add')
-			.end((err, res) => {
-				should.not.exist(err);
-				res.status.should.equal(200);
-				res.type.should.equal('text/html');
-				res.text.should.contain('Add Client');
-				done();
-			})
-	})
+  it('should get the add client form', done => {
+    chai
+      .request(server)
+      .get('/client/add')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('text/html');
+        res.text.should.contain('Add Client');
+        done();
+      });
+  });
 });
