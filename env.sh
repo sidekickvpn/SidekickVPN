cc27c44ba211eeed60752e487c71892e833e467f
Luke Baal <luke.baal@uoit.net>
2019-04-30 16:17:31 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,7 +0,0 @@
-# Network Variables
-export VPN_NET_INTERFACE="eth0"
-export VPN_NAME="wgnet0"
-export VPN_PORT="51820"
-export VPN_IP="192.168.10.1"
-export PORT="5000"
-export PUBLIC_IP="<public-ip>"
